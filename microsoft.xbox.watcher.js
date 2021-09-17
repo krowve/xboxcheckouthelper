8ecd3d388b17dc31e9975f3b89bb2250e00bdab3
krowve <84995512+krowve@users.noreply.github.com>
2021-09-17 17:23:15 -0400

    Create microsoft.xbox.watcher.js

---
+++ microsoft.xbox.watcher.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         microsoft.xbox.watcher
+// @namespace    http://tampermonkey.net/
+// @version      2021091701
+// @description  try to take over the world!
+// @author       You
+// @updateURL    https://raw.githubusercontent.com/krowve/xboxcheckouthelper/main/microsoft.xbox.watcher.js
+// @downloadURL  https://raw.githubusercontent.com/krowve/xboxcheckouthelper/main/microsoft.xbox.watcher.js
+// @match        https://www.microsoft.com/*
+// @match        https://www.xbox.com/*
+// @icon         https://www.google.com/s2/favicons?domain=microsoft.com
+// @grant        none
+// ==/UserScript==
+
+// Change this value to lengthen or shorten the time between refreshes
+var INTERVALTIME = 5000;
+var XBOXON = "https://www.myinstants.com/media/sounds/brian_xbox.mp3";
+
+(function() {
+
+var idVar = setInterval(function() {
+    var page = document.documentElement.innerHTML;
+    var pagetitle = String(document.title);
+
+    if (pagetitle.includes("Configure your Xbox Series X")) {
+        if (page.includes("Out of stock")) {
+            console.log("Out of stock");
+        } else {
+            console.log("Cart it");
+            window.open(XBOXON)
+        }
+        window.location = "https://www.xbox.com/en-us/configure/8wj714n3rbtl";
+    }
+
+},INTERVALTIME);
+})();
