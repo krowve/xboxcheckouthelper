0109183571193bb72ad6778e95fed03c56243d23
krowve <84995512+krowve@users.noreply.github.com>
2021-09-17 17:53:20 -0400

    Update microsoft.xbox.watcher.js

--- microsoft.xbox.watcher.js
+++ microsoft.xbox.watcher.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name         microsoft.xbox.watcher
 // @namespace    http://tampermonkey.net/
-// @version      2021091703
+// @version      2021091704
 // @description  try to take over the world!
 // @author       You
 // @updateURL    https://raw.githubusercontent.com/krowve/xboxcheckouthelper/main/microsoft.xbox.watcher.js
 // @downloadURL  https://raw.githubusercontent.com/krowve/xboxcheckouthelper/main/microsoft.xbox.watcher.js
-// @match        https://www.microsoft.com/*
 // @match        https://www.xbox.com/*
 // @icon         https://www.google.com/s2/favicons?domain=microsoft.com
 // @grant        none
