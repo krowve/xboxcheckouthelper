31e8f6c0428a70c986ae5042f3b4898f6de1b004
krowve <84995512+krowve@users.noreply.github.com>
2021-09-17 17:18:27 -0400

    Update microsoft.xbox.checkouthelper.js

--- microsoft.xbox.checkouthelper.js
+++ microsoft.xbox.checkouthelper.js
@@ -13,9 +13,7 @@
 // ==/UserScript==
 
 // Change this value to lengthen or shorten the time between refreshes
-var INTERVALTIME = 30000;
-var CHECKOUT = 1;
-var XBOXON = "https://www.myinstants.com/media/sounds/brian_xbox.mp3";
+var INTERVALTIME = 5000;
 var XBOXCOMMERCIAL = "https://www.myinstants.com/media/sounds/xbox-360-novo-comercial-no-brasil.mp3";
 
 (function() {
@@ -23,25 +21,7 @@ var XBOXCOMMERCIAL = "https://www.myinstants.com/media/sounds/xbox-360-novo-come
 var idVar = setInterval(function() {
     var page = document.documentElement.innerHTML;
     var pagetitle = String(document.title);
-    if (pagetitle.includes("Shopping cart - Microsoft Store")) {
-        if (page.includes("This item is out of stock")) {
-            console.log("Item out of stock");
-            CHECKOUT = 0;
-        }
-        if (page.includes("Your cart is empty")) {
-            console.log("Cart is empty");
-            CHECKOUT = 0;
-        }
-        if (page.includes("Xbox Series X")) {
-            console.log("Xbox in Cart");
-            if (CHECKOUT == 1) {
-                console.log("click checkout")
-                window.open(XBOXON)
-            }
-        }
-        CHECKOUT = 0;
-        window.location = "https://www.microsoft.com/en-us/store/cart";
-    }
+
     if (pagetitle.includes("Configure your Xbox Series X")) {
         if (page.includes("Out of stock")) {
             console.log("Out of stock");
