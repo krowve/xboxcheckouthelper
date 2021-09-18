92c6227adfabcd66e54938c9fe6892973e0acc37
krowve <84995512+krowve@users.noreply.github.com>
2021-09-17 21:17:19 -0400

    Update microsoft.xbox.watcher.js

--- microsoft.xbox.watcher.js
+++ microsoft.xbox.watcher.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         microsoft.xbox.watcher
 // @namespace    http://tampermonkey.net/
-// @version      2021091704
+// @version      2021091706
 // @description  try to take over the world!
 // @author       You
 // @updateURL    https://raw.githubusercontent.com/krowve/xboxcheckouthelper/main/microsoft.xbox.watcher.js
@@ -12,8 +12,9 @@
 // ==/UserScript==
 
 // Change this value to lengthen or shorten the time between refreshes
-var INTERVALTIME = 30000;
+var INTERVALTIME = 5000;
 var XBOXON = "https://www.myinstants.com/media/sounds/brian_xbox.mp3";
+var audio
 
 (function() {
 
@@ -26,7 +27,9 @@ var idVar = setInterval(function() {
             console.log("Out of stock");
         } else {
             console.log("Cart it");
-            window.open(XBOXON)
+            audio = document.createElement("audio");
+            audio.src = XBOXON;
+            audio.play();
         }
         window.location = "https://www.xbox.com/en-us/configure/8wj714n3rbtl";
     }
